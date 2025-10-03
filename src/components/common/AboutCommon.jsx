40888ebd0a054d731aa4f612aaf3db0c63ff743f
Antor Web-Dev <antorremeni@gmail.com>
2025-10-03 12:35:29 +0600

    chnage

--- src/components/common/AboutCommon.jsx
+++ src/components/common/AboutCommon.jsx
@@ -11,7 +11,7 @@ const AboutCommon = () => {
 
         <h1 className='text-[30px] font-medium font-popins text-[#000]'>Going all-in with millennial design</h1>
 
-        <div className='w-[817px]'><p className='text-[15px] font-normal font-popins text-[#9F9F9F]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. In nulla posuere sollicitudin aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus imperdiet. Libero enim sed faucibus turpis in. Cursus mattis molestie a iaculis at erat. Nibh cras pulvinar mattis nunc sed blandit libero. Pellentesque elit ullamcorper dignissim cras tincidunt. Pharetra et ultrices neque ornare aenean euismod elementum.</p></div>
+        <div className='w-[817px] mt-[12px] mb-[30px]'><p className='text-[15px] font-normal font-popins text-[#9F9F9F]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. In nulla posuere sollicitudin aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus imperdiet. Libero enim sed faucibus turpis in. Cursus mattis molestie a iaculis at erat. Nibh cras pulvinar mattis nunc sed blandit libero. Pellentesque elit ullamcorper dignissim cras tincidunt. Pharetra et ultrices neque ornare aenean euismod elementum.</p></div>
 
 
 <div className='pb-[12px] border-b w-[90px]'><Link  to={'/'} className='text-[16px] font-normal font-popins text-[#000]' >Read more</Link></div>
