ee6366395abff804690cd64d086e8756603bfca8
Antor Web-Dev <antorremeni@gmail.com>
2025-10-03 12:29:20 +0600

    chnage

--- src/components/common/AboutCommon.jsx
+++ src/components/common/AboutCommon.jsx
@@ -8,7 +8,9 @@ const AboutCommon = () => {
 
         <div className='w-full h-[500px]'><img src={aboutImg} alt="" /> </div>
 
-        <h1>Going all-in with millennial design</h1>
+        <h1 className='text-[30px] font-medium font-popins text-[#000]'>Going all-in with millennial design</h1>
+
+        <div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. In nulla posuere sollicitudin aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus imperdiet. Libero enim sed faucibus turpis in. Cursus mattis molestie a iaculis at erat. Nibh cras pulvinar mattis nunc sed blandit libero. Pellentesque elit ullamcorper dignissim cras tincidunt. Pharetra et ultrices neque ornare aenean euismod elementum.</p></div>
 
 
 
