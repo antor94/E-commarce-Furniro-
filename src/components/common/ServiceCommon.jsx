a6e3c5b1f6dfe98c9838f541f5a0a8d061d7cfbc
Antor Web-Dev <antorremeni@gmail.com>
2025-10-04 23:31:31 +0600

    chnage

--- src/components/common/ServiceCommon.jsx
+++ src/components/common/ServiceCommon.jsx
@@ -10,7 +10,7 @@ const ServiceCommon = () => {
 
         <div><img src={seImg} alt="" /></div>
         <div>
-            <h3>High Quality</h3>
+            <h3 className='text-[25px] font-semibold font-popins text-[#242424]'>High Quality</h3>
             <h5>crafted from top materials</h5>
         </div>
 
