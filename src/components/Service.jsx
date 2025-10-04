94773fda5db18fe84b771fc24c05b958c1f77cef
Antor Web-Dev <antorremeni@gmail.com>
2025-10-04 23:30:53 +0600

    chnage

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ServiceCommon from './common/ServiceCommon'
 
 const Service = () => {
 return (
@@ -9,25 +10,7 @@ return (
             <div className='flex items-center justify-between'>
 
 
-                {/* single */}
-                <div className='w-[376px]'>
-                    <h2 className='text-[32px] font-main font-medium text-black'>Free Delivery</h2>
-                    <p className='text-[20px] font-main font-medium text-[#9F9F9F]'>For all oders over $50, consectetur
-                        adipim scing elit.</p>
-                </div>
-                
-                 {/* single */}
-                <div className='w-[376px]'>
-                    <h2 className='text-[32px] font-main font-medium text-black'>90 Days Return</h2>
-                    <p className='text-[20px] font-main font-medium text-[#9F9F9F]'>If goods have problems, consectetur adipim scing elit.</p>
-                </div>
-
-
-                 {/* single */}
-                <div className='w-[376px]'>
-                    <h2 className='text-[32px] font-main font-medium text-black'>Secure Payment</h2>
-                    <p className='text-[20px] font-main font-medium text-[#9F9F9F]'>100% secure payment, consectetur adipim scing elit.</p>
-                </div>
+                <ServiceCommon />
 
 
 
---
+++ src/components/common/ServiceCommon.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import seImg from '../../assets/images/Service-customer.png'
+
+const ServiceCommon = () => {
+  return (
+    <>
+    
+    
+    <div >
+
+        <div><img src={seImg} alt="" /></div>
+        <div>
+            <h3>High Quality</h3>
+            <h5>crafted from top materials</h5>
+        </div>
+
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default ServiceCommon
